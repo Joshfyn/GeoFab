267004da0ecea374412fc849022be212782401e1
Joshfyn <joshuaadeegbe@yahoo.com>
2018-08-09 20:30:00 +0300

    UIupdater

--- app/src/main/java/com/example/jadeegbe/geofab/MainActivity.java
+++ app/src/main/java/com/example/jadeegbe/geofab/MainActivity.java
@@ -1,101 +1,93 @@
 package com.example.jadeegbe.geofab;
 
-import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.estimote.coresdk.common.config.EstimoteSDK;
-import com.estimote.coresdk.recognition.packets.Nearable;
-import com.estimote.coresdk.service.BeaconManager;
-
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
-    private BeaconManager beaconManager;
-    private String rssiVal;
-    boolean motionVAl;
     private static final String TAG = "BeaconID";
-    public TextView textView, textView1;
-
-
+    public static String RSSI_value_from_MyApplication = "";
+    public static String MOVING_value_from_MyApplication = "";
+    public TextView textView, textView1, textView2;
+    private Context mContect;
+    private Thread threadApplication;
+    private MyApplication myApplication;
+    public int refresh_interval = 1 * 1000;
+    public Handler uiRefresher = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mContect = this;
+
+
         textView = (TextView) findViewById(R.id.nextActivity);
         textView1 = (TextView) findViewById(R.id.nextActivity1);
+        textView2 = (TextView) findViewById(R.id.nextActivity2);
+
 
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if (bluetoothAdapter == null){
-            Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_SHORT).show();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Bluetooth Not Supported", Toast.LENGTH_SHORT).show();
         }
 
         if (!bluetoothAdapter.isEnabled()) {
-            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),1);
-            Toast.makeText(getApplicationContext(),"Turn ON Bluetooth to continue using APP",Toast.LENGTH_SHORT).show();
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 1);
+            Toast.makeText(getApplicationContext(), "Turn ON Bluetooth to continue using APP", Toast.LENGTH_SHORT).show();
 
         }
 
+        threadApplication = new Thread(myApplication);
+        threadApplication.start();
 
-        beaconManager = new BeaconManager(getApplicationContext());
-        beaconManager.setForegroundScanPeriod(500,0);
-        beaconManager.setBackgroundScanPeriod(500,0);
 
-        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-            @Override
-            public void onServiceReady() {
-                beaconManager.startNearableDiscovery();
-            }
-        });
+        uiRefresher.post(updateRSSI);
+        uiRefresher.post(updateMove);
 
-        beaconManager.setNearableListener(new BeaconManager.NearableListener() {
-            @Override
-            public void onNearablesDiscovered(List<Nearable> nearables) {
-                for (Nearable nearable : nearables){
-                    if (nearable.identifier.equals("6cf71fa481a5ae42")) {
-                        rssiVal = String.valueOf(nearable.rssi);
-                        textView.setText(rssiVal);
-                        Log.i(TAG, String.valueOf(rssiVal));
+    }
 
-                    }
-
-                    if (nearable.identifier.equals("2efe3b930fd5f6d2")) {
-                        motionVAl = nearable.isMoving;
-                        if (motionVAl == true){
-                            textView1.setText("It is moving");
-                        }
-                        else {
-                            textView1.setText("It is still");
-                        }
-                        Log.i(TAG, String.valueOf(motionVAl));
-
-                    }
+    Runnable updateRSSI =new Runnable() {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(RSSI_value_from_MyApplication);
+                    Log.i(TAG, String.valueOf(RSSI_value_from_MyApplication));
                 }
-            }
-        });
+            });
 
-        EstimoteSDK.initialize(this, "", "");
+            uiRefresher.postDelayed(updateRSSI,refresh_interval);
+        }
 
-    }
+    };
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        beaconManager.startNearableDiscovery();
-    }
+    Runnable updateMove =new Runnable() {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView1.setText(MOVING_value_from_MyApplication);
+                    Log.i(TAG, String.valueOf(MOVING_value_from_MyApplication));
+                }
+            });
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        beaconManager.stopNearableDiscovery();
-    }
+            uiRefresher.postDelayed(updateMove,refresh_interval);
+        }
+
+    };
 }
--- app/src/main/java/com/example/jadeegbe/geofab/MyApplication.java
+++ app/src/main/java/com/example/jadeegbe/geofab/MyApplication.java
@@ -1,31 +1,91 @@
 package com.example.jadeegbe.geofab;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
-import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.common.config.EstimoteSDK;
+import com.estimote.coresdk.recognition.packets.Nearable;
 import com.estimote.coresdk.service.BeaconManager;
 
-import java.util.UUID;
+import java.util.List;
 
 public class MyApplication extends Application implements Runnable{
-    private BeaconManager mBeaconManager;
+    private BeaconManager beaconManager;
+    boolean motionVAl;
+    private static final String TAG = "BeaconID";
+
+    private Context mcontext;
+    public String RssiVAL;
+    public String EstMoving;
+
+
+
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        /*mBeaconManager = new BeaconManager(getApplicationContext());
 
-        mBeaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+        beaconManager = new BeaconManager(getApplicationContext());
+        beaconManager.setForegroundScanPeriod(500,0);
+        beaconManager.setBackgroundScanPeriod(500,0);
+        EstimoteSDK.initialize(this, "fablab-monitor-gxs", "83a80186bf2d8fd3204757256f889d46");
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
             @Override
             public void onServiceReady() {
-                mBeaconManager.
+                beaconManager.startNearableDiscovery();
             }
-        });*/
+        });
+
+        beaconManager.setNearableListener(new BeaconManager.NearableListener() {
+            @Override
+            public void onNearablesDiscovered(List<Nearable> nearables) {
+                for (Nearable nearable : nearables){
+                    if (nearable.identifier.equals("6cf71fa481a5ae42")) {
+                        RssiVAL = String.valueOf(nearable.rssi);
+                        MainActivity.RSSI_value_from_MyApplication = RssiVAL;
+                        Log.i(TAG, String.valueOf(RssiVAL));
+
+                    }
+
+                    if (nearable.identifier.equals("2efe3b930fd5f6d2")) {
+                        motionVAl = nearable.isMoving;
+                        if (motionVAl == true){
+                            EstMoving = "It is moving";
+                        }
+                        else {
+                            EstMoving = "It is still";
+                        }
+                        MainActivity.MOVING_value_from_MyApplication = EstMoving;
+                        Log.i(TAG, String.valueOf(motionVAl));
+
+                    }
+                }
+            }
+        });
+
+
+
+
+
+
+
     }
 
     @Override
     public void run() {
-        
+        /*((MainActivity)mcontext).updateRSSI(RssiVAL);
+        ((MainActivity)mcontext).updateMoving(EstMoving);*/
+
+        try {
+            Thread.sleep(15);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 }
